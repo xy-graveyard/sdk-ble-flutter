6efef9fd528f63600a5dc5aefac37f4df123b86c
Carter Harrison <carterjharrison@gmail.com>
2019-07-25 15:53:35 -0700

    add write json

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
 mkdir ./flutter/.pub-cache/
-echo $GOOGLE_CREDS > ./flutter/.pub-cache/credentials.json 
+
+cat <<EOF > ./flutter/.pub-cache/credentials.json 
+{
+  "accessToken":"$accessToken",
+  "refreshToken":"$refreshToken",
+  "tokenEndpoint":"https://accounts.google.com/o/oauth2/token",
+  "scopes":["openid","https://www.googleapis.com/auth/userinfo.email"],
+  "expiration":1564090897608
+}
+EOF
+
 ./flutter/bin/flutter pub pub publish -f -v
