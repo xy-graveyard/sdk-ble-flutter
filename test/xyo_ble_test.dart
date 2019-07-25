da1e6c01f09cb12c02963da29c9ae6a12d78f926
Carter Harrison <carterjharrison@gmail.com>
2019-07-25 14:01:24 -0700

    fix flutter test

--- test/xyo_ble_test.dart
+++ test/xyo_ble_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sdk_ble_flutter/xyo_ble.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('xyo_ble');
