6f848dda26591cd28ba2400db97dcf596fe4c12d
Arie Trouw <arie.trouw@xy.company>
2019-09-11 09:16:28 -0700

    Building 3.0.11 for ios

--- lib/channels/bridge.dart
+++ lib/channels/bridge.dart
@@ -2,13 +2,24 @@ import 'package:sdk_ble_flutter/channels/node.dart';
 import 'package:sdk_ble_flutter/classes/archivist.dart';
 import 'package:sdk_ble_flutter/main.dart';
 
+typedef XyoStatusUpdatedCallback = void Function(String);
+
+enum status { none, enabled, bluetoothDisabled, bluetoothUnavailable, locationDisabled }
+
+//final stringToStatus()
+
 class XyoBridgeChannel extends XyoNodeChannel {
-  XyoBridgeChannel(String name) : super(name) {
-    events.receiveBroadcastStream();
+  XyoBridgeChannel(String name) : super(name);
+
+  Map<String, XyoStatusUpdatedCallback> onStatusUpdatedNotify = Map();
+
+  void reportStatusUpdate(dynamic status) {
+    //final bluetoothDevice = BluetoothDevice.fromBuffer(device);
+    //onStatusUpdatedNotify.forEach((String key, XyoStatusUpdatedCallback callback) => callback(status));
   }
 
   Future<String> getStatus() async {
-    return null;
+    return await invokeMethod('getStatus');
   }
 
   // Set the archivists
